5fb3a29ee9d10d5c0f7d96444e3c44a26724673b
Carlos-0723 <carlosa-cogollor@unilibre.edu.co>
2024-10-07 19:20:36 -0500

    Create login.js

---
+++ login.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBphWQaAKQXRdyFj7F-YR6s0aV9q_78300",
+    authDomain: "web1-9a055.firebaseapp.com",
+    projectId: "web1-9a055",
+    storageBucket: "web1-9a055.appspot.com",
+    messagingSenderId: "108770193462",
+    appId: "1:108770193462:web:ff732c67733308f5e16897",
+    measurementId: "G-ZJ35KTN8EK"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
